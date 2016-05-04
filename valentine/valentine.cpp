fc0a357841f5e11df8ca4ece0beec7d5230243a2
Patrick <revmydev@gmail.com>
2016-05-04 14:12:07 -0700

    Initial commit Not originally tracked with VCS. Program that outputs an embarrassing poem. Original didn't have timing for the output, but I added that in for effect.

---
+++ valentine/valentine.cpp
@@ -0,0 +1,19 @@
+// a geeky valentine
+#include <iostream>
+#include <conio.h>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+    cout << "Roses are #FF0000,\n";
+	Sleep(1500);
+    cout << "Violets are #0000FF,\n";
+	Sleep(1500);
+    cout << "The core of my CP\n";
+	Sleep(1500);
+    cout << "Gigahertz "; Sleep(375); cout << "for "; Sleep(375); cout << "U!\n";
+    
+    _getch();
+    return 0;
+}
